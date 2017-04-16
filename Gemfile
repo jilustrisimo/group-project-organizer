5d9439f0ea325d2448525abe4638ea59370d2a54
github username <jilustrisimo@users.noreply.github.com>
2017-04-16 19:14:47 -0400

    added 'activerecord-reset-pk-sequence' gem and seed data

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'omniauth-google'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'activerecord-reset-pk-sequence'
   gem 'byebug', platform: :mri
   gem 'dotenv-rails'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(email: Faker::Internet.safe_email,
+              username: Faker::Internet.user_name,
+              password: Faker::Internet.password(10, 20))
+end
+
+2.times do
+  Project.create(title: Faker::App.name,
+                 description: Faker::Lorem.paragraph,
+                 due_date: Faker::Time.forward(30))
+end
+
+7.times do
+  Task.create(title: Faker::Hipster.sentence(3),
+                 content: Faker::Lorem.paragraph,
+                 due_date: Faker::Time.forward(30))
+end
+
+Project.find_by_id(1).tasks << Task.all[0..5]
+Project.find_by_id(2).tasks << Task.all[6..-1]
