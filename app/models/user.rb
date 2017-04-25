b32f2a1728551f4b95630c2917211686ac5a877e
github username <jilustrisimo@users.noreply.github.com>
2017-04-24 22:28:54 -0400

    built .from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   validates :username, presence: true, uniqueness: true
 
   has_many :project_teams
   has_many :projects, through: :project_teams
   has_many :tasks
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.info.name
+    end
+  end
 end
