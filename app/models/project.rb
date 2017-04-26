6e5de6b440ed50c753ce2aebc2c357f725fc23d6
github username <jilustrisimo@users.noreply.github.com>
2017-04-25 23:28:59 -0400

    removed project_leader attr

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,5 @@
 class Project < ApplicationRecord
   has_one :project_team
-  has_one :project_leader, through: :project_team, source: :user
   has_many :project_members, through: :project_team, source: :user
   has_many :tasks, dependent: :destroy
 
