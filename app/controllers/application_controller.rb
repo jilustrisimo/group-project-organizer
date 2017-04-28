185957ce53a96cdd28f568325c57fdcd5b3b6f85
github username <jilustrisimo@users.noreply.github.com>
2017-04-27 23:20:16 -0400

    built check_if_team_member method in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def check_if_team_member
+    return if @project.team_members.include?(current_user)
+    flash[:error] = 'Only project team members can access this.'
+    redirect_to projects_path
+  end
+
   protected
 
   def configure_permitted_parameters
