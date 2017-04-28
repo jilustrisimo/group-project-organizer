ea5ab8aad4a0d836fff16215675e883f43666899
github username <jilustrisimo@users.noreply.github.com>
2017-04-27 22:11:05 -0400

    built set_project_team helper method and built project_teams#destroy action

--- app/controllers/project_teams_controller.rb
+++ app/controllers/project_teams_controller.rb
@@ -1,18 +1,13 @@
 class ProjectTeamsController < ApplicationController
-  before_action :set_project
 
   def create
+    @project = Project.find(params[:format])
     @project.team_members << current_user
     redirect_to projects_path, notice: "You Successfully joined #{@project.title}"
   end
 
   def destroy
-    
-  end
-
-  private
-
-  def set_project
-    @project = Project.find(params[:format])
+    ProjectTeam.find(params[:id]).destroy
+    redirect_to projects_path, notice: 'Successfully left project'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def set_project_team
+    @team = ProjectTeam.find_team_id(current_user, @project)
+  end
 end
