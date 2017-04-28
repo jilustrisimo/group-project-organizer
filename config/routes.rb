b18c809ab357f65bbcfe10ad010a481b8db9a9de
github username <jilustrisimo@users.noreply.github.com>
2017-04-28 15:40:27 -0400

    added comments to gemfile and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  root 'projects#index'
+
+  # display, add/remove routes for current_user.tasks
+
   get '/users/:user_id/tasks', to: 'tasks#index'
   post '/users/:user_id/tasks/:task_id/edit', to: 'tasks#assign'
   delete '/users/:user_id/tasks/:task_id', to: 'tasks#unassign'
-
-  root 'projects#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
