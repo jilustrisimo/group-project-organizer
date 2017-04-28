32bc43c17caf0d95d9f209cef2f534915d193fd5
github username <jilustrisimo@users.noreply.github.com>
2017-04-28 04:29:24 -0400

    refactored project_teams#destroy to remove relation between task and user

--- app/controllers/project_teams_controller.rb
+++ app/controllers/project_teams_controller.rb
@@ -3,11 +3,18 @@ class ProjectTeamsController < ApplicationController
   def create
     @project = Project.find(params[:format])
     @project.team_members << current_user
-    redirect_to projects_path, notice: "You Successfully joined #{@project.title}"
+    redirect_to @project, notice: "You Successfully joined #{@project.title}"
   end
 
   def destroy
-    ProjectTeam.find(params[:id]).destroy
+    pt = ProjectTeam.find(params[:id])
+    tasks = current_user.tasks.where('project_id = ?', pt.project_id.to_s)
+    # too many db impacts
+    tasks.each do |task|
+      task.user = nil
+      task.save
+    end
+    pt.destroy
     redirect_to projects_path, notice: 'Successfully left project'
   end
 end
