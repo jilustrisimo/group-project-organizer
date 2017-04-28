c20d4ff829164edd79420d4a74099cafcf3a9b44
github username <jilustrisimo@users.noreply.github.com>
2017-04-27 22:10:20 -0400

    built .find_team_id

--- app/models/project_team.rb
+++ app/models/project_team.rb
@@ -1,4 +1,9 @@
 class ProjectTeam < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  def self.find_team_id(user, project)
+    t = where('user_id = ? and project_id = ?', user.id.to_s, project.id.to_s)
+    t.pluck(:id).join
+  end
 end
