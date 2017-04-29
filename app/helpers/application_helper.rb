dec137670471516a107de6c85f50a39a3b59a06a
github username <jilustrisimo@users.noreply.github.com>
2017-04-28 22:34:15 -0400

    refactored ProjectTeam to use scope method, implemented in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def set_project_team
-    @team = ProjectTeam.find_team_id(current_user, @project)
+    @team = ProjectTeam.team_id(current_user, @project).pluck(:id).join
   end
 end
