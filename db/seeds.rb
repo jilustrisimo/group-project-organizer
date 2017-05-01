2d53bffde54cb4d25089e0373a67edf9bb817179
github username <jilustrisimo@users.noreply.github.com>
2017-05-01 10:49:32 -0400

    added data to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,11 +25,40 @@ Project.all.each do |project|
                         due_date: Faker::Date.between(Date.today, project.due_date))
            .save
   end
+
+  %w(1..3).sample.to_i.times do
+    project.tasks.sample.update(completed: true)
+  end
 end
 
-5.times do
-  User.all.each do |user|
+User.all.each do |user|
+  %w(2..4).sample.to_i.times do
     user.tasks << Task.all.sample
     user.tasks.last.project.team_members << user
   end
 end
+
+titles = ['Incompleted Project', 'Completed Project']
+descriptions = ['This project should be towards the top of your page as it is incomplete. Notice how projects and tasks are grouped by completion status then sorted by due date.', 'This project should be towards the bottom of the page and marked completed.  It should not be accessible as you are unable to join a completed project you are not already a member of. NOTE: if you see this at the top of your page after just creating this data hit refresh.  For some reason the order is reversed when data is generated the first time.']
+
+project = Project.create(title: titles[0], description: descriptions[0], due_date: Faker::Date.forward(5))
+
+project2 = Project.create(title: titles[1], description: descriptions[1], due_date: Faker::Date.forward(5))
+
+5.times do
+  project.tasks.build(title: Faker::Commerce.product_name,
+                      content: Faker::Hipster.paragraph,
+                      due_date: Faker::Date.between(Date.today, project.due_date))
+         .save
+end
+
+2.times do
+  project2.tasks.build(title: Faker::Commerce.product_name,
+                       content: Faker::Hipster.paragraph,
+                       due_date: Faker::Date.between(Date.today, project2.due_date),
+                       completed: true)
+          .save
+end
+
+project.tasks[0..1].each { |t| t.update(completed: true) }
+project2.tasks.each { |t| t.update(completed: true) }
