e8c0da02cad5e2cc0a1bac42a6c39f14474006bd
github username <jilustrisimo@users.noreply.github.com>
2017-05-15 22:57:54 -0400

    added attrs to projects serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,3 +1,7 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :description, :due_date, :completed
+  attributes :id, :description, :due_date, :completed, :updated_at
+
+  has_many :project_teams, dependent: :destroy
+  has_many :team_members, through: :project_teams, source: :user
+  has_many :tasks, dependent: :destroy
 end
