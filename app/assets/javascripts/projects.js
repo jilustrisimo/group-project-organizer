b82c644b6946c5f7341ad3cec2d1065dd7f8acdb
github username <jilustrisimo@users.noreply.github.com>
2017-05-15 23:16:56 -0400

    building Project constructor function

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -8,8 +8,21 @@ const bindClickHandlers = () => {
     $.get('/projects.json', projects => {
       $('.main').html('')
       $.each(projects, (idx,val) => {
-        console.log(val)
+        let project = new Project(val)
+        console.log(project)
       })
     })
   })
+}
+
+function Project(project) {
+  // id, description, due_date, completed, updated_at, project_teams, project_members, tasks
+  this.id = project.id
+  this.description = project.description
+  this.due_date = project.due_date
+  this.completed = project.completed
+  this.updated_at = project.updated_at
+  this.project_teams = project.project_teams
+  this.team_members = project.team_members
+  this.tasks = project.tasks
 }
