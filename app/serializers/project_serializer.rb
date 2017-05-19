43982054f09495ae51fe58a77f1dc48da738e53d
github username <jilustrisimo@users.noreply.github.com>
2017-05-19 11:48:36 -0400

    updated serializer to resolve issue with `?` in js

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,11 +1,17 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :completed?, :description, :due_date, :completed, :updated_at, :team_member?
+  attributes :id, :title, :is_completed, :description,
+             :due_date, :completed, :updated_at, :is_team_member
 
   has_many :project_teams, dependent: :destroy
   has_many :team_members, through: :project_teams, source: :user
   has_many :tasks, dependent: :destroy
 
-  def team_member?
+  def is_team_member
     object.team_member?(current_user)
   end
+
+  def is_completed
+    object.completed?
+  end
+
 end
