794a9684023f2008e51c82e4f62979ee57355e03
github username <jilustrisimo@users.noreply.github.com>
2017-05-19 11:24:22 -0400

    updated project_serializers to include more attrs

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,7 +1,11 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :due_date, :completed, :updated_at
+  attributes :id, :title, :completed?, :description, :due_date, :completed, :updated_at, :team_member?
 
   has_many :project_teams, dependent: :destroy
   has_many :team_members, through: :project_teams, source: :user
   has_many :tasks, dependent: :destroy
+
+  def team_member?
+    object.team_member?(current_user)
+  end
 end
